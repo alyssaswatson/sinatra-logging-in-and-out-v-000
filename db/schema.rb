c62edecfdbdf96ce95768b5f7879a801226f461e
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-12 23:23:00 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 1) do
   create_table "users", force: :cascade do |t|
     t.string  "username"
     t.string  "password"
-    t.float "balance"
+    t.integer "balance"
   end
 
 end
